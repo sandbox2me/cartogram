8b0115c7fd681b1b5b879363dfbfdf75c10d0ab8
Parris Khachi <parris@eventbrite.com>
2015-05-13 19:19:37 -0700

    Bumped Styleguide to 0.0.2

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,8 @@ module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt, { pattern: [
         'grunt-*'
     ]});
+
+    grunt.loadTasks('js-common/src/tasks');
     grunt.loadTasks('tasks');
 
 };
