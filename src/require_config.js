cd6f6bcfc3463d0bcfc86ed2d3b8e58d64a0dc3d
Parris Khachi <parris@eventbrite.com>
2015-05-20 16:44:31 -0700

    adding a require_config

---
+++ src/require_config.js
@@ -0,0 +1,55 @@
+var requireConfig = {
+
+    baseUrl: '',
+
+    paths: {
+        'q': './node_modules/q/q',
+        'pnltri': './node_modules/pnltri/pnltri',
+        'rbush': './node_modules/rbush/rbush',
+        'sinon': './node_modules/sinon/lib/sinon',
+        'three': './node_modules/three/three',
+        'tinycolor': './node_modules/tinycolor2/tinycolor'
+    },
+    shim: {
+        backbone: {
+            init: function(Backbone) {
+                window.Backbone = Backbone;
+            }
+        },
+        'three': {
+            'exports': 'THREE'
+        }
+    },
+    map: {
+        '*': {
+            'handlebars.runtime': 'handlebars/handlebars',
+            'backbone.wreqr': 'backbone_wreqr',
+            'backbone_advice': 'backbone.advice',
+            'backbone.marionette': 'marionette'
+        }
+    },
+    packages: [
+        {
+            name: 'js-common',
+            location: './node_modules/js-common/src'
+        },
+        {
+            name: 'cartogram',
+            location: './node_modules/cartogram/src',
+            main: './picasso'
+        }
+    ],
+};
+
+if (typeof require === 'undefined') {
+    // any values set on require before it loads will be used as config
+    // ignore the "redefinition error" due to the line below
+    window.require = window.requireConfig;
+} else if (typeof module === 'undefined') {
+    window.require.config(window.requireConfig);
+}
+
+// allow for loading in nodejs
+if (typeof module !== 'undefined' && module.exports) {
+    exports = module.exports = requireConfig;
+}
