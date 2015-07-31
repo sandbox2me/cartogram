61f5e98f30d43e7d63404ce79e31208960e065ea
Parris Khachi <parris@eventbrite.com>
2015-07-31 11:31:45 -0700

    Improved outputted requirejs definition

--- tasks/build.js
+++ tasks/build.js
@@ -4,23 +4,21 @@ module.exports = function(grunt) {
     var _ = require('underscore'),
         config = require('../src/require_config');
 
-    grunt.config.set('requirejs', {
-        dist: {
-            options: _.extend({}, config, {
-                baseUrl: '.',
-                name: 'main',
-                out: 'dist/cartogram.js',
-                findNestedDependencies: true,
-                useStrict: true,
-                // We should uglify in a separate process
-                // Optimizing here causes the build to take almost 4 minutes
-                optimize: 'none',
-                logLevel: 3,
-                generateSourceMaps: true,
-                preserveLicenseComments: true
-            })
-        }
-    });
+    grunt.config.set('requirejs.cartogram.options',
+        _.extend({}, config, {
+            baseUrl: '.',
+            name: 'src/cartogram',
+            out: 'dist/cartogram.js',
+            findNestedDependencies: true,
+            useStrict: true,
+            // We should uglify in a separate process
+            // Optimizing here causes the build to take almost 4 minutes
+            optimize: 'none',
+            logLevel: 3,
+            generateSourceMaps: true,
+            preserveLicenseComments: true
+        })
+    );
 
     grunt.registerTask('build', [
         'requirejs'
