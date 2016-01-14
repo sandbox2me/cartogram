fe549f247023b3ea74405e8b3a3928eb3b0de445
Alex Roberts <alex@eventbrite.com>
2016-01-14 15:21:31 -0800

    Use rectangle mesh construction logic from THREE

--- src/components/scene/builders/rectangle.js
+++ src/components/scene/builders/rectangle.js
@@ -28,25 +28,77 @@ class Rectangle {
     }
 
     _constructVertices() {
-        let positions = [
-            [-1.0, -1.0,  1.0],
-            [ 1.0, -1.0,  1.0],
-            [ 1.0,  1.0,  1.0],
+        // Based on THREE.PlaneBufferGeometry
+        // https://github.com/mrdoob/three.js/blob/master/src/extras/geometries/PlaneBufferGeometry.js
+        let width = 1;
+        let height = 1;
 
-            [ 1.0,  1.0,  1.0],
-            [-1.0,  1.0,  1.0],
-            [-1.0, -1.0,  1.0]
-        ];
-        let vertices = new Float32Array(6 * 3);
-        let i;
+        let width_half = width / 2;
+    	let height_half = height / 2;
 
-        for (i = 0; i < positions.length; i++) {
-            vertices[i * 3 + 0] = positions[i][0];
-            vertices[i * 3 + 1] = positions[i][1];
-            vertices[i * 3 + 2] = positions[i][2];
-        }
+    	let gridX = 1;
+    	let gridY = 1;
 
-        this.geometry.addAttribute('position', new BufferAttribute(vertices, 3));
+    	let gridX1 = gridX + 1;
+    	let gridY1 = gridY + 1;
+
+    	let segment_width = width / gridX;
+    	let segment_height = height / gridY;
+
+    	let vertices = new Float32Array(gridX1 * gridY1 * 3);
+    	let normals = new Float32Array(gridX1 * gridY1 * 3);
+    	let uvs = new Float32Array(gridX1 * gridY1 * 2);
+
+    	let offset = 0;
+    	let offset2 = 0;
+        let x, y, iy, ix;
+
+    	for (iy = 0; iy < gridY1; iy++) {
+
+    		y = iy * segment_height - height_half;
+
+    		for (ix = 0; ix < gridX1; ix++) {
+    			x = ix * segment_width - width_half;
+
+    			vertices[offset] = x;
+    			vertices[offset + 1] = - y;
+
+    			normals[offset + 2] = 1;
+
+    			uvs[offset2] = ix / gridX;
+    			uvs[offset2 + 1] = 1 - (iy / gridY);
+
+    			offset += 3;
+    			offset2 += 2;
+    		}
+    	}
+
+    	offset = 0;
+
+    	let indices = new ((vertices.length / 3) > 65535 ? Uint32Array : Uint16Array)(gridX * gridY * 6);
+    	for (iy = 0; iy < gridY; iy++) {
+    		for (ix = 0; ix < gridX; ix++) {
+    			let a = ix + gridX1 * iy;
+    			let b = ix + gridX1 * (iy + 1);
+    			let c = (ix + 1) + gridX1 * (iy + 1);
+    			let d = (ix + 1) + gridX1 * iy;
+
+    			indices[offset] = a;
+    			indices[offset + 1] = b;
+    			indices[offset + 2] = d;
+
+    			indices[offset + 3] = b;
+    			indices[offset + 4] = c;
+    			indices[offset + 5] = d;
+
+    			offset += 6;
+    		}
+    	}
+
+    	this.geometry.setIndex(new BufferAttribute(indices, 1));
+    	this.geometry.addAttribute('position', new BufferAttribute(vertices, 3));
+    	this.geometry.addAttribute('normal', new BufferAttribute(normals, 3));
+    	this.geometry.addAttribute('uv', new BufferAttribute(uvs, 2));
     }
 
     _attributes() {
