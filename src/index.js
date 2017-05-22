fe6916526de47b664a6e6acc1a2bfc409859aa6b
alex <alex@eventbrite.com>
2017-05-22 13:53:41 -0700

    Prevent lodash from escaping into the global

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+import _ from 'lodash';
 import Cartogram from './cartogram';
 import * as components from './components';
 import * as utils from './utils';
 
 // Merge in top level components into Cartogram library
 Object.assign(Cartogram, components, utils);
+
+
+// Make sure lodash doesn't escape onto the global
+_.noConflict();
 
 export default Cartogram;
